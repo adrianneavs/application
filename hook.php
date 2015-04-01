a6fe23226089fb7aecddd39b3c4ecc1198972101
hanafiah <ibnuyahya@gmail.com>
2015-04-01 09:55:39 +0800

    update hook

--- hook.php
+++ hook.php
@@ -4,4 +4,8 @@ $post_log = fopen("github_log.txt", "w");
 fwrite($post_log, $payload);
 fclose($post_log);
 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /www/git-repo/ && git reset --hard HEAD && git pull' );
+}
+
 echo 'received'.$payload;
