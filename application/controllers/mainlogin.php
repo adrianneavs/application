65883d1b99d6c3c430a8b9f3842fb5f02e9b7612
Adrianne Ajeng <adrianne.kreydle@gmail.com>
2015-04-09 17:56:02 +0800

    almost done

--- application/controllers/mainlogin.php
+++ application/controllers/mainlogin.php
@@ -1,4 +1,7 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 
 /*
@@ -14,212 +17,209 @@
  */
 class Mainlogin extends CI_Controller {
 
-    
-
-    public function __construct()
-   {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function index (){
+    public function index() {
         $this->loginform();
-        
+
         //something with () in the end is a function
-}
-
-public function cart(){
-    parent::Controller();
-    $this->load-model('m_products');
-}
-
-public function indexproduct(){
-    $data['products'] = $this->load->model('m_products')->update_prod();
-    print_r($data['products']);
-}
- public function loginform (){
-        $this->load->view('v_login');
-}
-
-public function order (){
-    //$this->load->view('v_members');
-    if ($this->session->userdata('is_logged_in')){
-        $this->load->view('v_food');
-    } else {
-        redirect('mainlogin/restricted');
-    }
-    
-}
-
-public function cart_update(){
-    
-if (isset($_POST["type"]) && $_POST["type"]=='add'){
-
-    $product_code = $this->input->post('product_code');
-    $product_qty = $this->input->post('product_qty');
-    $return_url = $this->input->post('return_url');
-    $product_name = $this->input->post('product_name');
-    $price = $this->input->post('price');
-$this->db->select('*');
-$this->db->from('products');
-$result = $this->db->get();
-$products = $result->result();
-
-if ($result) { 
-    $new_product = 
- ['name'=>$product_name, 
-                'code'=>$product_code, 
-                'price'=>$price, 
-                'qty'=>$product_qty];
-
-    if (isset($this->session->products)){
- $found = false;
- 
-   foreach ($this->session->products as $cart_itm)
- {
-     if($cart_itm["code"] == $product_code){
-         $product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"],'qty'=>$product_qty, 'price'=>$cart_itm["price"]);
-         $found = true;
-     } else {
-         $product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$cart_itm["qty"], 'price'=>$cart_itm["price"]);
-     }
- }
- if($found=false){
-     $this->session->products = array_merge($product, $new_product);
- }else{
-//found user item in array list, and increased the quantity
-$this->session->products = $product;
-}
-}else{
-$this->session->products = $new_product;    
-}
-    
-}
-    $view_data = array();
-        //load  m_products model
-        $this->load->model('m_products');
-
-        //past products data to view
-        $view_data['products'] = $this->m_products->get_products();
-        
-        //call view
-        $this->load->view('v_food', $view_data);
     }
 
-
-}
-public function confirm(){
-    if ($this->session->userdata('is_logged_in')){
-        $this->load->view('v_confirm');
-    } else {
-        redirect('mainlogin/restricted');
+    public function cart() {
+        parent::Controller();
+        $this->load - model('m_products');
     }
-}
 
-public function restricted(){
-    $this->load->view('v_restricted');
-}
+    public function indexproduct() {
+        $data['products'] = $this->load->model('m_products')->update_prod();
+        print_r($data['products']);
+    }
 
-public function login_valid(){
-    $this->load->library('form_validation');
-    $this->form_validation->set_rules('username','Username', 
-            'required|trim|xss_clean|callback_validate_credentials');
-    $this->form_validation->set_rules('password','Password', 'required|md5|trim');
-    
-    if ($this->form_validation->run()){
-        $data = array(
-            'username' => $this->input->post('username'), 'is_logged_in' => 1
-        );
-        $this->session->set_userdata($data);
-        redirect('mainlogin/order');
-    } else {
+    public function loginform() {
         $this->load->view('v_login');
     }
-}
 
-public function signup_valid(){
-    $this->load->library('form_validation');
-    $this->form_validation->set_rules('email','Email', 
-            'required|trim|valid_email|is_unique[users.email]');
-    $this->form_validation->set_rules('password','Password', 'required|trim|min_length[6]|max_length[30]');
-    $this->form_validation->set_rules('firstname','Firstname', 'required|alpha');
-    $this->form_validation->set_rules('lastname','Lastname', 'required|alpha');
-    $this->form_validation->set_rules('username','Username', 
-            'required|trim|is_unique[users.username]');
-    
-  
-    if ($this->form_validation->run()){
-$config['protocol'] = 'sendmail';
-$config['mailpath'] = '/usr/sbin/send
+    public function order() {
+        //$this->load->view('v_members');
+        if ($this->session->userdata('is_logged_in')) {
+            $this->load->view('v_food');
+        } else {
+            redirect('mainlogin/restricted');
+        }
+    }
+
+    public function cart_update() {
+        if ($this->session->userdata('is_logged_in')) {
+//load  m_products model
+            $this->load->model('m_products');
+
+            if (isset($_POST["addtocart"])) {
+
+                $product_id = $this->input->post('id');
+                $product_qty = $this->input->post('product_qty');
+                $product = $this->m_products->get_product($product_id); //
+                //echo $product_id." ".$product_qty;
+                //echo $product->product_desc;
+                $cart_session = array();
+                if ($this->session->userdata('cart') !== FALSE) {
+                    $cart_session = $this->session->userdata('cart');
+                }
+                $cart_session[$product_id] = [
+                    'name' => $product->product_name,
+                    'code' => $product->product_code,
+                    'price' => $product->price * $product_qty,
+                    'qty' => $product_qty
+                ];
+                //print_r($cart_session);
+                $this->session->set_userdata('cart', $cart_session);
+
+//
+//            if (isset($this->session->products)) {
+//                $found = false;
+//
+//                foreach ($this->session->products as $cart_itm) {
+//                    if ($cart_itm["code"] == $product_code) {
+//                        $product[] = array('name' => $cart_itm["name"], 'code' => $cart_itm["code"], 'qty' => $product_qty, 'price' => $cart_itm["price"]);
+//                        $found = true;
+//                    } else {
+//                        $product[] = array('name' => $cart_itm["name"], 'code' => $cart_itm["code"], 'qty' => $cart_itm["qty"], 'price' => $cart_itm["price"]);
+//                    }
+//                }
+//                if ($found = false) {
+//                    $this->session->products = array_merge($product, $new_product);
+//                } else {
+////found user item in array list, and increased the quantity
+//                    $this->session->products = $product;
+//                }
+//            } else {
+//                $this->session->products = $new_product;
+//            }
+            }
+            $view_data = array();
+            $view_data['cart'] = $this->session->userdata('cart');
+
+            //past products data to view
+            $view_data['products'] = $this->m_products->get_products();
+
+            //call view
+            $this->load->view('v_food', $view_data);
+        } else {
+            redirect('mainlogin/restricted');
+        }
+    }
+
+    public function confirm() {
+        if ($this->session->userdata('is_logged_in')) {
+            $this->load->view('v_confirm');
+        } else {
+            redirect('mainlogin/restricted');
+        }
+    }
+
+    public function restricted() {
+        $this->load->view('v_restricted');
+    }
+
+    public function login_valid() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('username', 'Username', 'required|trim|xss_clean|callback_validate_credentials');
+        $this->form_validation->set_rules('password', 'Password', 'required|md5|trim');
+
+        if ($this->form_validation->run()) {
+            $data = array(
+                'username' => $this->input->post('username'), 'is_logged_in' => 1
+            );
+            $this->session->set_userdata($data);
+            redirect('mainlogin/order');
+        } else {
+            $this->load->view('v_login');
+        }
+    }
+
+    public function signup_valid() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]|max_length[30]');
+        $this->form_validation->set_rules('firstname', 'Firstname', 'required|alpha');
+        $this->form_validation->set_rules('lastname', 'Lastname', 'required|alpha');
+        $this->form_validation->set_rules('username', 'Username', 'required|trim|is_unique[users.username]');
+
+
+        if ($this->form_validation->run()) {
+            $config['protocol'] = 'sendmail';
+            $config['mailpath'] = '/usr/sbin/send
     mail';
-$this->email->initialize($config);        
+            $this->email->initialize($config);
 //generate random key
-        $key = md5(uniqid());
-        $email = $this->input->post('email');
-        $this->load->library('email');
-        //$email = $this->session->userdata('email');
-        //$email_code = $this->email_code;
-        $this->load->model('m_user');
-        
-        $this->email->set_mailtype('html');
-        $this->email->from('admin@orenjiebi.com', "Admin");
-        $this->email->to($email);
-        $this->email->subject('Confirm your account');
-        
-        $message = "<p>Thanks a bunch of love for signing up.</p>";
-        $message .= "<p><a href='".base_url()."main/register_user/$key'>Click me</a> to confirm your account</p>";
-                     
-        $this->email->message($message);
-        
-        if ($this->m_user->prosessignup($key)){
-        if ($this->email->send()){
-        echo "email has been sent";
-        print_r ($this->load->view('v_login'));
-        } else echo "email cannot be sent";
-         } else echo "failed add to database";
-        
-        
-    } else {
-  $this->load->view('v_signup');
-    }
-    //if ($this->form_validation->run()){
-      //  echo "yes";
-    //}else {
-      //  echo "no";
-        //$this->load->view('v_signup');
-    //}
-    
-}
+            $key = md5(uniqid());
+            $email = $this->input->post('email');
+            $this->load->library('email');
+            //$email = $this->session->userdata('email');
+            //$email_code = $this->email_code;
+            $this->load->model('m_user');
 
-public function validate_credentials(){
-    $this->load->model('m_user');
-    
-    if ($this->m_user->proseslogin()){
-        return true;
+            $this->email->set_mailtype('html');
+            $this->email->from('admin@orenjiebi.com', "Admin");
+            $this->email->to($email);
+            $this->email->subject('Confirm your account');
+
+            $message = "<p>Thanks a bunch of love for signing up.</p>";
+            $message .= "<p><a href='" . base_url() . "main/register_user/$key'>Click me</a> to confirm your account</p>";
+
+            $this->email->message($message);
+
+            if ($this->m_user->prosessignup($key)) {
+                if ($this->email->send()) {
+                    echo "email has been sent";
+                    print_r($this->load->view('v_login'));
+                } else
+                    echo "email cannot be sent";
+            } else
+                echo "failed add to database";
+        } else {
+            $this->load->view('v_signup');
+        }
+        //if ($this->form_validation->run()){
+        //  echo "yes";
+        //}else {
+        //  echo "no";
+        //$this->load->view('v_signup');
+        //}
+    }
+
+    public function validate_credentials() {
+        $this->load->model('m_user');
+
+        if ($this->m_user->proseslogin()) {
+            return true;
         } else {
             $this->form_validation->set_message('validate_credentials', 'PLEASE enter correct username and password');
             return false;
         }
-    
-}
+    }
 
-public function userlogout(){
-    $this->session->sess_destroy();
-    redirect('mainlogin/loginform');
-}
+    public function userlogout() {
+        $this->session->sess_destroy();
+        redirect('mainlogin/loginform');
+    }
 
-public function signup(){
-    $this->load->view('v_signup');
-}
+    public function signup() {
+        $this->load->view('v_signup');
+    }
+
 //public function hi (){
-  //  $this->load->model('m_user');
-        
-        /*    if($param1=FALSE){
-                $view_data['msg'] = 'PArameter 2 is required';
-            }else{*/
+    //  $this->load->model('m_user');
+
+    /*    if($param1=FALSE){
+      $view_data['msg'] = 'PArameter 2 is required';
+      }else{ */
     //    $view_data = array();
-      //  $view_data['detailuser'] = $this->m_user->get_users($username); 
-        //$this->load->view('v_members', $view_data);
+    //  $view_data['detailuser'] = $this->m_user->get_users($username); 
+    //$this->load->view('v_members', $view_data);
 //}
 //put your code here
 }
+
 ?>
--- application/views/v_food.php
+++ application/views/v_food.php
@@ -7,40 +7,38 @@ and open the template in the editor.
 <html>
     <head>
         <?php
-        
-$currency = '$'; //Currency sumbol or code
+        $currency = '$'; //Currency sumbol or code
+        ?>
 
-?>
-       
-                <style type="text/css">
+        <style type="text/css">
             h1 {
-		color: #444;
-		background-color: transparent;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+                color: #444;
+                background-color: transparent;
+                font-size: 19px;
+                font-weight: normal;
+                margin: 0 0 14px 0;
+                padding: 14px 15px 10px 15px;
+            }
 
             body {
-		background-color: #add8e6;
-		margin: 40px;
-		font: 15px/20px Arial;
-		color: #4F5155;
-	}
+                background-color: #add8e6;
+                margin: 40px;
+                font: 15px/20px Arial;
+                color: #4F5155;
+            }
             #body{
-		margin: 0 15px 0 15px;
-	}
-            
-            
-        
+                margin: 0 15px 0 15px;
+            }
+
+
+
             signup_submit{
-            color: white;
-            border-radius: 4px;
-            text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-            background: rgb(28, 184, 65);
-        }
-	</style>
+                color: white;
+                border-radius: 4px;
+                text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+                background: rgb(28, 184, 65);
+            }
+        </style>
 
         <meta charset="UTF-8">
         <title></title>
@@ -48,77 +46,55 @@ $currency = '$'; //Currency sumbol or code
     <body>
         <h1>HI</h1>
         <h1>Products</h1>
-    <div class="products">
-    <?php
-    $currency = '$'; //Currency symbol or code
-$this->db->select('*');
-$this->db->from('products');
-$result = $this->db->get();
-$products = $result->result();
-  
-	
-        //fetch results set as object and output HTML
-        foreach ($products as $product){
-			echo '<div class="product">'; 
-            echo form_open('/mainlogin/cart_update');
-            //echo '<form method="post" action="cart_update">';
-            echo '<div class="product-thumb"><img src="D:/KREYDLEPROJ/ajenglocal/'.$product->product_img_name.'"></div>';
-            echo '<div class="product-content"><h3>'.$product->product_name.'</h3>';
-            echo '<div class="product-desc">'.$product->product_desc.'</div>';
-            echo '<div class="product-info">';
-			echo 'Price '.$currency.$product->price.' | ';
-            echo 'Qty <select name="product_qty">'
-                        . '<option value="1">1</option>'
-                        . '<option value="2">2</option>'
-                        . '</select>';
-			echo '<button class="add_to_cart">Add To Cart</button>';
-			echo '</div></div>';
-            echo '<input type="hidden" name="product_name" value="'.$product->product_name.'" />';
-            echo '<input type="hidden" name="product_desc" value="'.$product->product_desc.'" />';
-            echo '<input type="hidden" name="price" value="'.$product->price.'" />';
-            echo '<input type="hidden" name="product_code" value="'.$product->product_code.'" />';
-            echo '<input type="hidden" name="type" value="add" />';
-		echo form_close();	
-            //echo '</form>';
-            echo '</div>';
-        
-    
-        }
-    ?>
-        <h2>Cart</h2>
-        <?php
+        <div class="products">
+            <?php
+            $currency = '$'; //Currency symbol or code
+//fetch results set as object and output HTML
+            foreach ($products as $product) {
+                echo '<div class="product">';
+                echo form_open('/mainlogin/cart_update');
+                //echo '<form method="post" action="cart_update">';
+                echo '<div class="product-thumb"><img src="D:/KREYDLEPROJ/ajenglocal/' . $product->product_img_name . '"></div>';
+                echo '<div class="product-content"><h3>' . $product->product_name . '</h3>';
+                echo '<div class="product-desc">' . $product->product_desc . '</div>';
+                echo '<div class="product-info">';
+                echo 'Price ' . $currency . $product->price . ' | ';
+                echo 'Qty <select name="product_qty">'
+                . '<option value="1">1</option>'
+                . '<option value="2">2</option>'
+                . '</select>';
+                echo '<button class="add_to_cart" name="addtocart">Add To Cart</button>';
+                echo '</div></div>';
+                echo '<input type="hidden" name="id" value="' . $product->id . '" />';
 
-if(isset($this->session->products))
-{
-    $total = 0;
-    echo '<ol>';
-$this->db->select('*');
-$this->db->from('products');
-$result = $this->db->get();
-$products = $result->result();
 
-    foreach ($this->session->products as $cart_itm)
-    {
+                echo form_close();
+                //echo '</form>';
+                echo '</div>';
+            }
+            ?>
+            <h2>Cart</h2>
+            <?php
+            foreach ($cart as $cart_itm) {
+                $total = 0;
+                echo '<li class="cart-itm">';
 
-        echo '<li class="cart-itm">';
-        echo '<span class="remove-itm"><a href="order"='.$cart_itm["product_code"].'&return_url='.$current_url.'">&times;</a></span>';
-        echo '<h3>'.$cart_itm["product_name"].'</h3>';
-        echo '<div class="p-code">P code : '.$cart_itm["product_code"].'</div>';
-        echo '<div class="p-qty">Qty : '.$cart_itm["qty"].'</div>';
-        echo '<div class="p-price">Price :'.$currency.$cart_itm["price"].'</div>';
-        echo '</li>';
-        $subtotal = ($cart_itm["price"]*$cart_itm["qty"]);
-        $total = ($total + $subtotal);
-    }
-    echo '</ol>';
-    echo '<span class="check-out-txt"><strong>Total : '.$currency.$total.'</strong> <a href="view_cart.php">Check-out!</a></span>';
-	echo '<span class="empty-cart"><a href="cart_update.php?emptycart=1&return_url='.$current_url.'">Empty Cart</a></span>';
-}else{
-    echo 'Your Cart is empty';
-}
-?>
+                echo '<h3>' . $cart_itm["name"] . '</h3>';
+                echo '<div class="p-code">P code : ' . $cart_itm["code"] . '</div>';
+                echo '<div class="p-qty">Qty : ' . $cart_itm["qty"] . '</div>';
+                echo '<div class="p-price">Price :' . $currency . $cart_itm["price"] . '</div>';
+                echo '</li>';
+                $subtotal = ($cart_itm["price"] * $cart_itm["qty"]);
+                $total = ($total + $subtotal);
+            }
+            echo '</ol>';
 
-        </br></br>
-        <a href ='<?php echo base_url()."mainlogin/userlogout"?>'>LOGOUT</a>
+            echo '<span class="check-out-txt"><strong>Total : ' . $currency . $total . '</strong> </br> <a href="view_cart.php">Check-out!</a></span>';
+            echo '</br>';
+            echo '<span class="empty-cart"><a href="cart_update.php?emptycart=1&return_url="> Empty Cart</a></span>';
+            ?>
+
+            </br></br>
+            <a href ='<?php echo base_url() . "mainlogin/userlogout" ?>'>LOGOUT</a>
     </body>
 </html>
