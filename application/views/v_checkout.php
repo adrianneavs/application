d006bf7022ee32b068445a424f62c386d6a30ac7
Adrianne Ajeng <adrianne.kreydle@gmail.com>
2015-04-17 13:43:35 +0800

    committt

--- application/views/v_checkout.php
+++ application/views/v_checkout.php
@@ -27,7 +27,7 @@ and open the template in the editor.
             <div class ="col-md-4"></div>
             <div class ="col-md-4">
                 <div class="alert alert-success" role="alert">Your order has been placed</div><br>
-            <a href ="<?php echo base_url() . "mainlogin/save_order";?>">Order History</a>
+            
             <a href ="<?php echo base_url() . "mainlogin/index";?>">Shop Again!</a>
             </div>
             <div class ="col-md-4"></div></div>
