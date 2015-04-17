8d9a4aaca6b5c0012425c8114f68418a61de93ed
Adrianne Ajeng <adrianne.kreydle@gmail.com>
2015-04-17 11:02:48 +0800

    commit 3

--- application/views/v_success.php
+++ application/views/v_success.php
@@ -1,8 +1,11 @@
 <html>
     <head>
         <title>Codeigniter cart class</title>
-        <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <style>
             body {
 
