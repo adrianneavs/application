71da1f6592d938dea4d62168b11d38630bfe9866
Adrianne Ajeng <adrianne.kreydle@gmail.com>
2015-04-17 13:46:12 +0800

    committt

--- application/views/v_checkout.php
+++ application/views/v_checkout.php
@@ -28,7 +28,7 @@ and open the template in the editor.
             <div class ="col-md-4">
                 <div class="alert alert-success" role="alert">Your order has been placed</div><br>
             
-                <a href ="<?php echo base_url() . "mainlogin/index";?>"><span class="glyphicon glyphicon-cart">Shop Again!</span></a>
+                <span class="glyphicon glyphicon-cart"><a href ="<?php echo base_url() . "mainlogin/index";?>">Shop Again!</a></span>
             </div>
             <div class ="col-md-4"></div></div>
             <?php
