f7e2e79cb80d8de38dcecc25d04a9a6e6df7669e
Adrianne Ajeng <adrianne.kreydle@gmail.com>
2015-04-17 14:25:24 +0800

    committt

--- application/views/v_success.php
+++ application/views/v_success.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Codeigniter cart class</title>
-        
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -22,7 +22,7 @@
         </style>
     </head>
     <body>
-        <?php echo $username;?>
+        <?php echo $username; ?>
         <div class="jumbotron">
             <h2 align="center">Order History</h2>
         </div>
@@ -73,7 +73,7 @@
                             <?php
                             echo anchor('mainlogin/deleteorder/' . $value['id'] . '/' . $orderid . '/' . $value['rowid'], 'X');
                             ?>
-                                        <!--                        <input type="submit" onclick="window.location = 'deleteorder'" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></input>-->
+                                                <!--                        <input type="submit" onclick="window.location = 'deleteorder'" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></input>-->
                         </td>
 
 
@@ -90,11 +90,13 @@
         </table>
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <?php echo form_open('mainlogin/checkout');?>
-            <input type="button" name="checkout" class="btn btn-primary" value="Checkout">
+            
+            <form class="form-horizontal" action="<?php echo base_url() . "mainlogin/checkout"; ?>" method="post">
+                <input type="submit" name="checkout" class="btn btn-primary" value="Checkout">
 
-            </input>
-            <?php echo form_close();?>
+                </input>
+            </form>
+            
         </div>
         <div class="col-md-4"></div>
 
